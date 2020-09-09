68d829b7453d8692c34fbd511e3d1d4d582ed147
Takaya Nagai <takaya.nagai@jp.ricoh.com>
2020-09-09 09:04:51 +0900

    一旦typescriptやめる

--- index.js
+++ index.js
@@ -1,108 +1,84 @@
-import {
-    ErrorHandler,
-    HandlerInput,
-    RequestHandler,
-    SkillBuilders,
-  } from 'ask-sdk-core';
-  import {
-    Response,
-    SessionEndedRequest,
-  } from 'ask-sdk-model';
+const Alexa = require('ask-sdk-core');
 
-  const LaunchRequestHandler : RequestHandler = {
-    canHandle(handlerInput : HandlerInput) : boolean {
-      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-    },
-    handle(handlerInput : HandlerInput) : Response {
-      const speechText = 'ようこそ、アレクサスキルキットへ。こんにちは、と言ってみてください。';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .withSimpleCard('ハローワールド', speechText)
-        .getResponse();
-    },
-  };
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'ようこそ、アレクサスキルキットへ。こんにちは、と言ってみてください。';
 
-  const HelloWorldIntentHandler : RequestHandler = {
-    canHandle(handlerInput : HandlerInput) : boolean {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-    },
-    handle(handlerInput : HandlerInput) : Response {
-      const speechText = 'こんにちは';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .withSimpleCard('ハローワールド', speechText)
-        .getResponse();
-    },
-  };
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('ハローワールド', speechText)
+      .getResponse();
+  }
+};
 
-  const HelpIntentHandler : RequestHandler = {
-    canHandle(handlerInput : HandlerInput) : boolean {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput : HandlerInput) : Response {
-      const speechText = 'こんにちは、と言ってみてください。';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .withSimpleCard('ハローワールド', speechText)
-        .getResponse();
-    },
-  };
+const HelloWorldIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'こんにちは';
 
-  const CancelAndStopIntentHandler : RequestHandler = {
-    canHandle(handlerInput : HandlerInput) : boolean {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-          || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput : HandlerInput) : Response {
-      const speechText = 'さようなら';
-  
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .withSimpleCard('ハローワールド', speechText)
-        .withShouldEndSession(true)      
-        .getResponse();
-    },
-  };
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('ハローワールド', speechText)
+      .getResponse();
+  }
+};
 
-  const SessionEndedRequestHandler : RequestHandler = {
-    canHandle(handlerInput : HandlerInput) : boolean {
-      return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-    },
-    handle(handlerInput : HandlerInput) : Response {
-      console.log(`次の理由でセッションが終了しました。 ${(handlerInput.requestEnvelope.request as SessionEndedRequest).reason}`);
-  
-      return handlerInput.responseBuilder.getResponse();
-    },
-  };
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'こんにちは、と言ってみてください。';
 
-  const ErrorHandler : ErrorHandler = {
-    canHandle(handlerInput : HandlerInput, error : Error ) : boolean {
-      return true;
-    },
-    handle(handlerInput : HandlerInput, error : Error) : Response {
-      console.log(`処理されたエラー：${error.message}`);
-  
-      return handlerInput.responseBuilder
-        .speak('すみません。コマンドを理解できませんでした。もう一度お願いします。')
-        .reprompt('すみません。コマンドを理解できませんでした。もう一度お願いします。')
-        .getResponse();
-    },
-  };
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('ハローワールド', speechText)
+      .getResponse();
+  }
+};
 
-  let skill;
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'さようなら';
 
-exports.handler = async (event, context) => {
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('ハローワールド', speechText)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    //クリーンアップロジックをここに追加します
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+let skill;
+
+exports.handler = async function (event, context) {
   console.log(`REQUEST++++${JSON.stringify(event)}`);
   if (!skill) {
-    skill = SkillBuilders.custom()
+    skill = Alexa.SkillBuilders.custom()
       .addRequestHandlers(
         LaunchRequestHandler,
         HelloWorldIntentHandler,
